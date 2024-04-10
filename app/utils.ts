d92b90e45e384c254d030c35e09d7d9c5af3c762
CJ <chiajien@gmail.com>
2024-04-10 17:12:32 +0800

    open vision for gpt4

--- app/utils.ts
+++ app/utils.ts
@@ -291,7 +291,7 @@ export function getMessageImages(message: RequestMessage): string[] {
 
 export function isVisionModel(model: string) {
   // Note: This is a better way using the TypeScript feature instead of `&&` or `||` (ts v5.5.0-dev.20240314 I've been using)
-  const visionKeywords = ["vision", "claude-3"];
+  const visionKeywords = ["vision", "claude-3", "gpt-4"];
 
   return visionKeywords.some((keyword) => model.includes(keyword));
 }
