c42688d6ba56667a0c3e8cd124d8bc94e2bcbc5b
CJ <chiajien@gmail.com>
2024-06-01 18:20:36 +0800

    add login ui

--- app/page.tsx
+++ app/page.tsx
@@ -11,16 +11,47 @@ import { Home } from "./components/home";
 
 import { getServerSideConfig } from "./config/server";
 import { IconButton } from "./components/button";
-import { CustomSignInButton } from "./components/signInBtn";
 
 const serverConfig = getServerSideConfig();
 
 export default async function App() {
+  const buttonStyle: any = {
+    padding: "30px 30px",
+    border: "none",
+    borderRadius: "40px",
+    fontSize: "20px",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    cursor: "pointer",
+    transition: "all 0.3s ease-in-out",
+    boxShadow: "0 6px 20px rgba(0, 0, 0, 0.2)",
+    display: "inline-block",
+    background: "linear-gradient(90deg, #FFA07A, #87CEEB)",
+  };
+
   return (
     <>
       <ClerkProvider>
         <SignedOut>
-          <SignInButton />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              minHeight: "100vh",
+              flexDirection: "column",
+              fontFamily: "Arial, sans-serif",
+            }}
+          >
+            <h1
+              style={{ fontSize: "80px", marginBottom: "50px", color: "#333" }}
+            >
+              Welcome to Techy GPT
+            </h1>
+            <SignInButton>
+              <button style={buttonStyle}>Click Me To Sign In</button>
+            </SignInButton>
+          </div>
         </SignedOut>
         <SignedIn>
           <Home />
